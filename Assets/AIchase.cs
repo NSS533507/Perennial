b0574aed24c17cfba6e00b53783760f556b1160e
NEEL SUTHAR <104153057+NSS533507@users.noreply.github.com>
2025-01-25 13:00:34 +0530

    push

--- Assets/AIchase.cs
+++ Assets/AIchase.cs
@@ -4,24 +4,28 @@ public class AIchase : MonoBehaviour
 {
     public GameObject character;
     public float speed;
+    public float stopDistance = 3.0f; // Minimum distance to stop chasing
 
     private float distance;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position,character.transform.position );
-        distance -= 5;
-        Vector2 direction = character.transform.position - transform.position;
+        distance = Vector2.Distance(transform.position, character.transform.position);
 
-        direction.Normalize();
-        float angle = Mathf.Atan2(direction.x,direction.y) * Mathf.Rad2Deg;
-        transform.position = Vector2.MoveTowards(this.transform.position,character.transform.position,speed * Time.deltaTime);
-        transform.rotation = Quaternion.LookRotation(Vector3.forward * angle);
+        if (distance > stopDistance)
+        {
+            Vector2 direction = character.transform.position - transform.position;
+            direction.Normalize();
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.position = Vector2.MoveTowards(transform.position, character.transform.position, speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
     }
 }
